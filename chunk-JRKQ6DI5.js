b26064b54906162f55eb19546803fea87079990f
deepak <deepaksmail2004@gmail.com>
2024-07-12 03:49:08 +0530

    h

---
+++ chunk-JRKQ6DI5.js
@@ -0,0 +1,1 @@
+import"./chunk-JNJ3D7EP.js";var t=[{path:"",loadComponent:()=>import("./chunk-ZXDR7SYE.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
---
+++ chunk-RRD7G2MV.js
@@ -0,0 +1,1 @@
+import"./chunk-JNJ3D7EP.js";var o=[{path:"",loadComponent:()=>import("./chunk-GTG3ASNH.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
